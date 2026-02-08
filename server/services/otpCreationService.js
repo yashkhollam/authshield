b1c344304f7bfe15f659a5d1dcaa61c500c9039b
yashkhollam <yashkhollam01@gmail.com>
2026-02-08 13:53:11 +0530

    test commit

--- server/services/otpCreationService.js
+++ server/services/otpCreationService.js
@@ -1,73 +1,33 @@
+import nodemailer from "nodemailer";
+import bcrypt from "bcrypt";
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.Email_user,
+    pass: process.env.Email_apppass,
+  },
+});
 
-import nodemailer from 'nodemailer'
-import bcrypt from 'bcrypt'
-import {Resend} from 'resend'
-// import userdatamodel from '../models/userdata.js'
+const otpCreationService = async (user) => {
+  const otp = Math.floor(100000 + Math.random() * 900000);
 
+  user.hashotp = await bcrypt.hash(otp.toString(), 10);
+  user.otpExpiresAt = new Date(Date.now() + 3 * 60 * 1000);
+  user.otpAttempts = 0;
+  user.lastOtpSendAt = new Date();
+  await user.save();
 
-     const resend = new Resend(process.env.RESEND_API_KEY);
-
-const otpCreationService=async(user)=>{
-   
-  
-    const otp=Math.floor(100000+Math.random()*900000)
-
-    const hashotp=await bcrypt.hash(otp.toString(),10)
-    const otpExpiresAt= new Date(Date.now()+3*60*1000)
-
-   
-          user.otpAttempts=0
-       
-          user.hashotp=hashotp,
-          user.otpExpiresAt=otpExpiresAt
-          user.lastOtpSendAt=new Date
-          await user.save()
-
-
-     try{
-    // const otpAttempts=
-
-//           const transporter=nodemailer.createTransport({
-//                host:process.env.Email_host,
-//                port:process.env.Email_port,
-//                secure:true,
-//                auth:{
-//                  user:process.env.Email_user,
-//                  pass:process.env.Email_apppass,
-//                }
-               
-//           })
-   
-//          const info= await transporter.sendMail({
-//            from:`"AUTHSHIELD" ${process.env.Email_user}`,
-//            to:user.email,
-//            subject:"Otp verification from AUTHSHIELD",
-//            html:`<h2>Your verification code is  ${otp}</h2>
-//                  <h3>This code will expires in 1 minutes</h3>`
-//           })
-
-//         //   await transporter.sendMail(otoption)
-   
-// console.log("Email sent:", info.response);
-
-      const response=await  resend.emails.send({
-    from:`Authshield <${process.env.Email_user}> `,
-    //<onboarding@resend.dev>
+  transporter.sendMail({
+    from: `"Authshield" <${process.env.Email_user}>`,
     to: user.email,
-    subject: "Otp verification from AUTHSHIELD",
+    subject: "OTP Verification",
     html: `<h2>Your OTP is ${otp}</h2><p>Valid for 3 minutes</p>`
-  });
-     
-  console.log("otp creation success: ",response)
-  
-    }
-    catch(err){
-      console.log("OTP email failed",err.message)
-    
-    }
- return {success:true}
-    
-}
+  })
+  .then(() => console.log("OTP sent"))
+  .catch(err => console.error("OTP failed:", err.message));
 
-export default otpCreationService
+  return { success: true };
+};
+
+export default otpCreationService;
