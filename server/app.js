d24f6cd241efb89ec4048bf66d587b2d4a726bcf
yashkhollam <yashkhollam01@gmail.com>
2026-02-08 09:57:03 +0530

    update app.js in backend

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const PORT=process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-      origin:"https://authshield-chi.vercel.app",
+      origin:"https://authshield-omega.vercel.app",
       credentials:true
 }))
 app.use(cookieParser())
