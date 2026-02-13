c9aabd6f9fe3871c0604033fdaee9381c5496909
yashkhollam <yashkhollam01@gmail.com>
2026-02-13 14:11:45 +0530

    update frontend url

--- server/app.js
+++ server/app.js
@@ -14,8 +14,8 @@ const PORT=process.env.PORT || 5000
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    //   origin:"https://authshield-theta.vercel.app",
-    origin:"http://localhost:5173",
+      origin:"https://authshield-sigma.vercel.app",
+    // origin:"http://localhost:5173",
       credentials:true
 }))
 
